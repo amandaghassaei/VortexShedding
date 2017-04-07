e77412b68e51891fe75dc07d91dc75103681bd22
amandaghassaei <amandaghassaei@gmail.com>
2017-04-07 13:14:31 -0400

    removed a test

--- GPUMath.js
+++ GPUMath.js
@@ -15,30 +15,6 @@ function initGPUMath(){
     }
     gl.disable(gl.DEPTH_TEST);
 
-    // check if we can render to floating point textures.
-    var tex = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, tex);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, null);
-
-    // some drivers have a bug that requires you turn off filtering before
-    // rendering to a texture.
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-    // make a framebuffer
-    var fb = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-
-    // attach the texture
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
-                            gl.TEXTURE_2D, tex, 0);
-
-    // check if we can render
-    var status = gl.checkFramebufferStatus(gl.FRAMEBUFFER);
-    if (status != gl.FRAMEBUFFER_COMPLETE) {
-        console.warn("can't render to floating point textures");
-    }
-
     var maxTexturesInFragmentShader = gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS);
     console.log(maxTexturesInFragmentShader + " textures max");
 
